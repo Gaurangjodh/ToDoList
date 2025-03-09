c114ea879203e9434644c63280f9901ce03922b1
Gaurangjodh <jodhgaurang@gmail.com>
2025-03-09 13:34:00 +0530

    Setup the initial repo for project.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import home from './home';
-import about from './about';
-import menu from './menu';
-import contact from './contact';
-import './styles.css';
-home();
-function loadButtons(){
-    const homeBtn = document.querySelector("#home");
-    const aboutBtn = document.querySelector("#about");
-    const menuBtn = document.querySelector("#menu");
-    const contactBtn = document.querySelector("#contact");
-    aboutBtn.addEventListener('click', (e) => {
-        about();
-    });
-    homeBtn.addEventListener('click', (e) => {
-        home();
-    })
-    menuBtn.addEventListener('click', (e) => {
-        menu();
-    })
-    contactBtn.addEventListener('click', (e)=> {
-        contact();
-    })
-}
-loadButtons();
+// import home from './home';
+// import about from './about';
+// import menu from './menu';
+// import contact from './contact';
+// import './styles.css';
+// home();
+// function loadButtons(){
+//     const homeBtn = document.querySelector("#home");
+//     const aboutBtn = document.querySelector("#about");
+//     const menuBtn = document.querySelector("#menu");
+//     const contactBtn = document.querySelector("#contact");
+//     aboutBtn.addEventListener('click', (e) => {
+//         about();
+//     });
+//     homeBtn.addEventListener('click', (e) => {
+//         home();
+//     })
+//     menuBtn.addEventListener('click', (e) => {
+//         menu();
+//     })
+//     contactBtn.addEventListener('click', (e)=> {
+//         contact();
+//     })
+// }
+// loadButtons();
